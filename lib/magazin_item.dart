ea85ca81cc5c70038c5e8713b36f4a5121844977
IbrahimElseginy <160798019+ibrahimelseginy@users.noreply.github.com>
2024-11-21 14:35:35 +0000

    Update magazin_item.dart

--- lib/magazin_item.dart
+++ lib/magazin_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/category.dart';
+import 'package:flutter_basics/category.dart';
+import 'package:flutter_basics/magazine_details_screen.dart';
 
 class MagazineItem extends StatelessWidget {
   Category category;
@@ -19,20 +20,68 @@ class MagazineItem extends StatelessWidget {
             width: double.infinity,
             fit: BoxFit.cover,
           ),
-          Container(
-              margin: const EdgeInsets.only(right: 4, bottom: 6),
-              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
-              color: const Color(0xFF8529BA).withOpacity(0.5),
-              child: Text(
-                category.title,
-                style: const TextStyle(color: Colors.white, fontSize: 18),
-              ))
+          // Container(
+          //   margin: const EdgeInsets.only(right: 4, bottom: 6),
+          //   padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
+          //   color: const Color(0xFF8529BA).withOpacity(0.5),
+          //   child: Text(
+          //     category.title,
+          //     style: const TextStyle(color: Colors.white, fontSize: 18),
+          //   ),
+          // ),
+
+          // Buttons
+
+          // first button
+
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (ctx) => const MagazineDetailsScreen(),
+              ));
+              // Navigator.of(context).pushReplacement();
+              // Navigator.of(context).pushReplacementNamed('Magazine_details');
+              // Navigator.of(context).pushNamed(MagazineDetailsScreen.routeName);
+            },
+
+            style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF8529BA).withOpacity(0.5)),
+            child: const Text(
+              'Car',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            // style: ButtonStyle(),
+          )
+          /*
+          // second button
+          TextButton( 
+            onPressed: () {},
+            child: Text('login'),
+            // style: ElevatedButton.styleFrom(),
+            // style: ButtonStyle(),)
+          )
+   
+          // third button
+          OutlinedButton(
+            onPressed: () {},
+            child: Text('login'),
+            // style: ElevatedButton.styleFrom(),
+            // style: ButtonStyle(),)
+          )  
+          // fourth button
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.access_alarm_rounded,
+              color: Colors.amber,
+            ),
+          )*/
         ],
       ),
     );
   }
 
-  void login() {
-    //login logic
-  }
+  // void login() {
+  //   //login logic
+  // }
 }
