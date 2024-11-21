210a76fdcb665776b348d073577a26e216b99e87
IbrahimElseginy <160798019+ibrahimelseginy@users.noreply.github.com>
2024-11-21 14:35:01 +0000

    Update title.dart

--- lib/title.dart
+++ lib/title.dart
@@ -8,13 +8,18 @@ class TitleWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          padding: const EdgeInsets.symmetric(vertical: 2),
-          // width: 180,
-          color: const Color(0xFF8529BA),
-          child: Text(text,
-              style: const TextStyle(color: Colors.white, fontSize: 22),
-              textAlign: TextAlign.center)),
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        padding: const EdgeInsets.symmetric(vertical: 2),
+        color: const Color(0xFF8529BA),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
